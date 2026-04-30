5673cb8eb644ed8675908adb8a9051481e3971ac
Tarun Uggina <tarun.uggina@gmail.com>
2026-04-30 21:14:23 +0530

    Added explaination for var and let keyswords
    Added explaination for var and let keyswords

--- LearningSwiftUI.swift
+++ LearningSwiftUI.swift
@@ -9,3 +9,6 @@
 //I am starting the swiftui...
 // var t = "Tarun will become pro in ios development."
 //print(t)
+//"var", intialsing with this keyword can be chnaged any time.
+//If we intialise with "let", keyword we can not chnage the value of particular varible, it throws error.
+//let v = "varun", can't be chnaged.
