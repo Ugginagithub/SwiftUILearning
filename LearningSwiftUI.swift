0d4566cd9f02c7ba0d197fa4dac86f79855bd1ab
Tarun Uggina <tarun.uggina@gmail.com>
2026-04-30 23:19:02 +0530

    Learned about var, let, int , double and strings
    Learned about var, let, int , double and strings

--- LearningSwiftUI.swift
+++ LearningSwiftUI.swift
@@ -10,5 +10,40 @@
 // var t = "Tarun will become pro in ios development."
 //print(t)
 //"var", intialsing with this keyword can be chnaged any time.
+
+
 //If we intialise with "let", keyword we can not chnage the value of particular varible, it throws error.
-//let v = "varun", can't be chnaged.
+//let v = "varun", can't be chnaged. let does not let use to chnage its value.
+// so that we will not change the values by accident.
+
+
+//Now lets learn about how to create the strings.
+// we can create a multi- line strings.
+/*
+            let myString = """
+                            This is a
+                            multi line
+                            string
+                           """
+
+            print(myString)
+ 
+ */
+
+// for length of the string we can use : myString.count
+// to make the string uppercase : myString.uppercased()
+// to check the sufix and prefix : myString.hasPrefix("hi"), myString.hasSuffix(".jpg")
+
+//Now lets learn how to store the whole numbers
+//we can use the underscore to represnt comas for bigger number like 10,000 -> 10_000(in swift).
+//swift takes 10_000 as 10000, for human readable language we can use _ for readability.
+
+//let n = 10_000
+//print(n) //10000
+// we can perform mathematical operations +,-,*,/
+// we can check the multiple also , eg : print(number.isMultiple(of: 3)) // gives boolean.
+
+
+
+
+
